fbbfec1a255ff11c59aa59e9db9b30ec4ece10e4
Sandesh Acharya <mysandesh@gmail.com>
2024-09-25 23:34:54 +0545

    Dashboard Layout made responsive

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -14,9 +14,13 @@ export default function DashboardLayout({
   return (
     <div className="h-screen flex">
       {/*LEFT */}
-      <div className="w-1/6">L</div>
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200">
+        L
+      </div>
       {/*RIGHT */}
-      <div className="w-5/6">R</div>
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-200">
+        R
+      </div>
     </div>
   );
 }
