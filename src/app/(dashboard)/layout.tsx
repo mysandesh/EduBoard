25319983437d12996df37ecc33212302fdf53ffd
Sandesh Acharya <mysandesh@gmail.com>
2024-10-01 22:39:32 +0545

    Logo added

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "../globals.css";
+import Image from "next/image";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Education Board Management Dashboard",
@@ -15,7 +17,13 @@ export default function DashboardLayout({
     <div className="h-screen flex">
       {/*LEFT */}
       <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200">
-        L
+        <Link
+          href="/"
+          className="flex items-center justify-center lg:justify-start gap-2"
+        >
+          <Image src="/logo.png" alt="logo" width={32} height={32} />
+          <span className="hidden lg:block">EduBoard</span>
+        </Link>
       </div>
       {/*RIGHT */}
       <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-200">
