98e12c5821587d92dc8cd833b8f1b4c286668bb1
Sandesh Acharya <mysandesh@gmail.com>
2024-12-08 18:26:11 +0545

    Padding added to logo

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -17,7 +17,7 @@ export default function DashboardLayout({
   return (
     <div className="h-screen flex">
       {/*LEFT */}
-      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200">
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200" p-4>
         <Link
           href="/"
           className="flex items-center justify-center lg:justify-start gap-2"
