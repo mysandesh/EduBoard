18fc33c2a40dd6544d4ed6cf1c9fe8d168a4b460
Sandesh Acharya <mysandesh@gmail.com>
2024-12-09 17:01:35 +0545

    Margins fix

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -104,13 +104,17 @@ const Menu = () => {
     <div className="mt-4 text-sm">
       {menuItems.map((i) => (
         <div className="flex flex-col gap-2" key={i.title}>
-          <span className="hidden lg:block text-gray-400 font-light my-4">
+          <span className="hidden lg:block text-gray-400 font-light my-4 mx-1">
             {i.title}
           </span>
           {i.items.map((item) => (
-            <Link href={item.href} key={item.label}>
+            <Link
+              href={item.href}
+              key={item.label}
+              className="flex items-center justify-center lg:justify-start gap-2 text-gray-500 py-2 px-1"
+            >
               <Image src={item.icon} alt="" width={20} height={20} />
-              <span>{item.label}</span>
+              <span className="hidden lg:block">{item.label}</span>
             </Link>
           ))}
         </div>
