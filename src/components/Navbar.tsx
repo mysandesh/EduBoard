19a0cb500beaab7ef26618962b6416a784a1b348
Sandesh Acharya <mysandesh@gmail.com>
2024-12-10 21:34:26 +0545

    Username and Avatar added on navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,6 +16,17 @@ const Navbar = () => {
         <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer">
           <Image src="/announcement.png" alt="" width={20} height={20} />
         </div>
+        <div className="flex flex-col">
+          <span className="text-xs leading-3 font-medium">John Doe</span>
+          <span className="text-[10px] text-gray-500 text-right">Admin</span>
+        </div>
+        <Image
+          src="/avatar.png"
+          alt=""
+          width={36}
+          height={36}
+          className="rounded-full"
+        />
       </div>
     </div>
   );
