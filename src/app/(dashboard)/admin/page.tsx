6eaef43754ba0616b7b1cdb0f314ca87f5392741
Sandesh Acharya <mysandesh@gmail.com>
2024-12-20 20:53:40 +0545

    Logo and Menu padding fix

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,12 @@
 const AdminPage = () => {
-  return <div className="">AdminPage</div>;
+  return (
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
+      {/* LEFT */}
+      <div className=""></div>
+      {/* RIGHT */}
+      <div className=""></div>
+    </div>
+  );
 };
 
 export default AdminPage;
