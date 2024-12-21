00120cf6883727ccd4d2c037a15ca9a7c64f54df
Sandesh Acharya <mysandesh@gmail.com>
2024-12-22 00:44:29 +0545

    Add Chart percentage fix for Students

--- src/components/CountChart.tsx
+++ src/components/CountChart.tsx
@@ -16,12 +16,12 @@ const data = [
   },
   {
     name: "Girls",
-    count: 54,
+    count: 50,
     fill: "#8884d8",
   },
   {
     name: "Boys",
-    count: 46,
+    count: 50,
     fill: "#83a6ed",
   },
 ];
@@ -40,9 +40,9 @@ const CountChart = () => {
           <RadialBarChart
             cx="50%"
             cy="50%"
-            innerRadius="10%"
-            outerRadius="80%"
-            barSize={10}
+            innerRadius="40%"
+            outerRadius="100%"
+            barSize={32}
             data={data}
           >
             <RadialBar
@@ -56,16 +56,14 @@ const CountChart = () => {
       {/* BOTTOM  */}
       <div className="flex justify-center gap-16">
         <div className="flex flex-col gap-1">
-          <div className="w-5 h-5 bg-customSky rounded-full">
-            <h1 className="font-bold">1,234</h1>
-            <h2 className="text-xs text-gray-300">Boys (55%)</h2>
-          </div>
+          <div className="w-5 h-5 bg-customSky rounded-full"></div>
+          <h1 className="font-bold">1,234</h1>
+          <h2 className="text-xs text-gray-300">Boys (55%)</h2>
         </div>
         <div className="flex flex-col gap-1">
-          <div className="w-5 h-5 bg-customYellow rounded-full">
-            <h1 className="font-bold">1,234</h1>
-            <h2 className="text-xs text-gray-300">Girls (45%)</h2>
-          </div>
+          <div className="w-5 h-5 bg-customYellow rounded-full"></div>
+          <h1 className="font-bold">1,234</h1>
+          <h2 className="text-xs text-gray-300">Girls (45%)</h2>
         </div>
       </div>
     </div>
