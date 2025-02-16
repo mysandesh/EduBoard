d4552c891520d88a91607e9ec6489fc36f0f571e
Sandesh Acharya <mysandesh@gmail.com>
2025-02-16 08:14:16 +0545

    FinanceChart enabled

--- src/components/FinanceChart.tsx
+++ src/components/FinanceChart.tsx
@@ -15,45 +15,75 @@ import {
 
 const data = [
   {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
+    name: "Jan",
+    income: 4000,
+    expense: 2400,
     amt: 2400,
   },
   {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
+    name: "Feb",
+    income: 3000,
+    expense: 1398,
     amt: 2210,
   },
   {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
+    name: "Mar",
+    income: 2000,
+    expense: 9800,
     amt: 2290,
   },
   {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
+    name: "Apr",
+    income: 2780,
+    expense: 3908,
     amt: 2000,
   },
   {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
+    name: "May",
+    income: 1890,
+    expense: 4800,
     amt: 2181,
   },
   {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
+    name: "Jun",
+    income: 2390,
+    expense: 3800,
     amt: 2500,
   },
   {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
+    name: "Jul",
+    income: 3490,
+    expense: 4300,
+    amt: 2100,
+  },
+  {
+    name: "Aug",
+    income: 2490,
+    expense: 3300,
+    amt: 1100,
+  },
+  {
+    name: "Sep",
+    income: 3690,
+    expense: 4900,
+    amt: 2800,
+  },
+  {
+    name: "Oct",
+    income: 3490,
+    expense: 4300,
+    amt: 2100,
+  },
+  {
+    name: "Nov",
+    income: 3790,
+    expense: 4200,
+    amt: 2500,
+  },
+  {
+    name: "Dec",
+    income: 3490,
+    expense: 4300,
     amt: 2100,
   },
 ];
@@ -65,6 +95,32 @@ const FinanceChart = () => {
         <h1 className="text-sm font-semibold">Finance</h1>
         <Image src="/moreDark.png" alt="" width={12} height={12} />
       </div>
+      <ResponsiveContainer width="100%" height="90%">
+        <LineChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line
+            type="monotone"
+            dataKey="income"
+            stroke="#8884d8"
+            activeDot={{ r: 8 }}
+          />
+          <Line type="monotone" dataKey="expense" stroke="#82ca9d" />
+        </LineChart>
+      </ResponsiveContainer>
     </div>
   );
 };
