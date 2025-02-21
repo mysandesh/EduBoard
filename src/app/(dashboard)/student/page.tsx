4cc527d29bf7a2bc9e22a7c3395d25c4c5ffdd7f
Sandesh Acharya <mysandesh@gmail.com>
2025-02-21 21:17:32 +0545

    Student page flex fixed

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -1,5 +1,5 @@
 const StudentPage = () => {
-  return <div className="">StudentPage</div>;
+  return <div className="p-4 flex gap-4 flex-col xl:flex-row">StudentPage</div>;
 };
 
 export default StudentPage;
