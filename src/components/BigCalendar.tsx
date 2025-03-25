85536521ecec1874e3bd49e8a0a0eb31c6117f70
Sandesh Acharya <mysandesh@gmail.com>
2025-03-25 18:46:20 +0545

    BigCalendar week days fix

---
+++ src/components/BigCalendar.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { Calendar, momentLocalizer, Views, View } from "react-big-calendar";
+import moment from "moment";
+import { calendarEvents } from "@/lib/data";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import { useState } from "react";
+
+const localizer = momentLocalizer(moment);
+
+const BigCalendar = () => {
+  const [view, setView] = useState<View>(Views.WORK_WEEK);
+
+  const handleOnChangeView = (selectedView: View) => {
+    setView(selectedView);
+  };
+
+  return (
+    <div>
+      <Calendar
+        localizer={localizer}
+        events={calendarEvents}
+        startAccessor="start"
+        endAccessor="end"
+        views={["work_week", "day"]}
+        view={view}
+        style={{ height: 500 }}
+        onView={handleOnChangeView}
+      />
+    </div>
+  );
+};
+
+export default BigCalendar;
