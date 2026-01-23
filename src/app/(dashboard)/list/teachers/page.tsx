63f955bafa727834e694c93d958d6dcc52dead1b
Sandesh Acharya <mysandesh@gmail.com>
2026-01-23 15:06:42 +0545

    Teacher List page icons placement styles applied

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -9,9 +9,15 @@ const TeacherListPage = () => {
         <h1 className="hidden md:block text-lg font-semibold">All Teachers</h1>
         <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
           <TableSearch />
-          <div className="">
-            <button>
+          <div className="flex items-center gap-4 self-end">
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-customYellow">
               <Image src="/filter.png" alt="" width={14} height={14} />
+            </button>
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-customYellow">
+              <Image src="/sort.png" alt="" width={14} height={14} />
+            </button>
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-customYellow">
+              <Image src="/plus.png" alt="" width={14} height={14} />
             </button>
           </div>
         </div>
