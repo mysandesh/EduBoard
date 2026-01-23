769a572ae87332e3e9c6c28dc5b7787fd15758f6
Sandesh Acharya <mysandesh@gmail.com>
2026-01-23 14:22:21 +0545

    Teacher List page icons placement add

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -1,9 +1,20 @@
+import TableSearch from "@/components/TableSearch";
+import Image from "next/image";
+
 const TeacherListPage = () => {
   return (
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
       {/*TOP */}
-      <div className="div">
-        <h1>All Teachers</h1>
+      <div className="flex items-center justify-between">
+        <h1 className="hidden md:block text-lg font-semibold">All Teachers</h1>
+        <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
+          <TableSearch />
+          <div className="">
+            <button>
+              <Image src="/filter.png" alt="" width={14} height={14} />
+            </button>
+          </div>
+        </div>
       </div>
       {/*LIST */}
       <div className="div"></div>
