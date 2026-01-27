7532c1c01b584589db586ac6be372c551f2431a7
Sandesh Acharya <mysandesh@gmail.com>
2026-01-27 15:59:45 +0545

    Image and Table data display design applied

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -55,18 +55,21 @@ const columns = [
 
 const TeacherListPage = () => {
   const renderRow = (item: Teacher) => (
-    <tr key={item.id}>
-      <td>
+    <tr
+      key={item.id}
+      className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-customPurpleLight"
+    >
+      <td className="flex items-center gap-4 p-4">
         <Image
           src={item.photo}
           alt=""
           width={40}
           height={40}
-          className="md:hidden xl:block"
+          className="md:hidden xl:block
           w-10
           h-10
           rounded-full
-          object-cover
+          object-cover"
         />
         <div className="flex flex-col">
           <h3 className="font-semibold">{item.name}</h3>
