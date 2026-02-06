9ee3918a8f6632d27b9e30ffd17bd50e30eaecf2
Sandesh Acharya <mysandesh@gmail.com>
2026-02-06 14:03:54 +0545

    Teacher individual page design initiated

---
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -0,0 +1,12 @@
+const SingleTeacherPage = () => {
+  return (
+    <div className="flex-1 -4 flex flex-col gap-4 xl:flex-row">
+      {/* LEFT */}
+      <div className="w-full xl:w-2/3">Left</div>
+      {/* RIGHT */}
+      <div className="w-full xl:w-1/3">Right</div>
+    </div>
+  );
+};
+
+export default SingleTeacherPage;
