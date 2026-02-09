aa003a476b149e329b06b4d5e34e2136cc3dcb00
Sandesh Acharya <mysandesh@gmail.com>
2026-02-09 19:38:13 +0545

    Teacher schedule area assigned

--- src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -2,7 +2,17 @@ const SingleTeacherPage = () => {
   return (
     <div className="flex-1 -4 flex flex-col gap-4 xl:flex-row">
       {/* LEFT */}
-      <div className="w-full xl:w-2/3">Left</div>
+      <div className="w-full xl:w-2/3">
+        {/* TOP */}
+        <div className="">
+          {/* USER INFO CARD */}
+          <div></div>
+          {/* SMALL CARDS */}
+          <div></div>
+        </div>
+        {/* BOTTOM */}
+        <div className="">Schedule</div>
+      </div>
       {/* RIGHT */}
       <div className="w-full xl:w-1/3">Right</div>
     </div>
