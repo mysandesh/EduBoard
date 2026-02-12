bae20d67c51227cc41a27cb5908bbef80743ed89
Sandesh Acharya <mysandesh@gmail.com>
2026-02-12 09:39:27 +0545

    Teacher profile cards padding fix

--- src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -1,6 +1,6 @@
 const SingleTeacherPage = () => {
   return (
-    <div className="flex-1 -4 flex flex-col gap-4 xl:flex-row">
+    <div className="flex-1 p-4 flex flex-col gap-4 xl:flex-row">
       {/* LEFT */}
       <div className="w-full xl:w-2/3">
         {/* TOP */}
