597a0745bebadc9d50c3f3aae2d23b54fc55c32f
Sandesh Acharya <mysandesh@gmail.com>
2026-03-22 11:50:26 +0545

    User image dimensions added

--- src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 const SingleTeacherPage = () => {
   return (
     <div className="flex-1 p-4 flex flex-col gap-4 xl:flex-row">
@@ -7,7 +9,15 @@ const SingleTeacherPage = () => {
         <div className="flex flex-col lg:flex-row gap-4">
           {/* USER INFO CARD */}
           <div className="bg-customSky py-6 px-4 rounded-md flex-1 flex gap-4">
-            <div className="w-1/3"></div>
+            <div className="w-1/3">
+              <Image
+                src=""
+                alt=""
+                width={144}
+                height={144}
+                className="w-36 h-36 rounded-full"
+              />
+            </div>
             <div className="w-2/3"></div>
           </div>
           {/* SMALL CARDS */}
